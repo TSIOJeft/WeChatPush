7692b1f1ca897299cfcd405e6cafee40cab6d6da
TSIOJeft <1971214855@qq.com>
2022-09-03 22:48:30 +0800

    commit

--- itchat/storage/messagequeue.py
+++ itchat/storage/messagequeue.py
@@ -1,4 +1,5 @@
 import logging
+
 try:
     import Queue as queue
 except ImportError:
@@ -8,9 +9,11 @@ from .templates import AttributeDict
 
 logger = logging.getLogger('itchat')
 
+
 class Queue(queue.Queue):
     def put(self, message):
         queue.Queue.put(self, Message(message))
+
 
 class Message(AttributeDict):
     def download(self, fileName):
@@ -18,15 +21,18 @@ class Message(AttributeDict):
             return self.text(fileName)
         else:
             return b''
+
     def __getitem__(self, value):
         if value in ('isAdmin', 'isAt'):
-            v = value[0].upper() + value[1:] # ''[1:] == ''
+            v = value[0].upper() + value[1:]  # ''[1:] == ''
             logger.debug('%s is expired in 1.3.0, use %s instead.' % (value, v))
             value = v
         return super(Message, self).__getitem__(value)
+
     def __str__(self):
         return '{%s}' % ', '.join(
-            ['%s: %s' % (repr(k),repr(v)) for k,v in self.items()])
+            ['%s: %s' % (repr(k), repr(v)) for k, v in self.items()])
+
     def __repr__(self):
         return '<%s: %s>' % (self.__class__.__name__.split('.')[-1],
-            self.__str__())
+                             self.__str__())
